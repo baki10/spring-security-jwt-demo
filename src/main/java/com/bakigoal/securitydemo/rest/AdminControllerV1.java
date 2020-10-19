ba9a3eae1ed6a310959deb96e8f4835e72b8cea9
Ilmir Bakirov <bakirov@xrm.ru>
2020-10-19 14:28:17 +0500

    add some rest

---
+++ src/main/java/com/bakigoal/securitydemo/rest/AdminControllerV1.java
@@ -0,0 +1,30 @@
+package com.bakigoal.securitydemo.rest;
+
+import com.bakigoal.securitydemo.dto.UserDto;
+import com.bakigoal.securitydemo.model.User;
+import com.bakigoal.securitydemo.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin/")
+@AllArgsConstructor
+public class AdminControllerV1 {
+
+    private final UserService userService;
+
+    @GetMapping("users/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Long id) {
+        User user = userService.findById(id);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(UserDto.fromUser(user), HttpStatus.OK);
+    }
+}
